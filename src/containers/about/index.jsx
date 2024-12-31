368f408388559222d6d7a90f0c81764df4460b12
aarti <arti9783@gmail.com>
2024-12-31 17:20:05 +0530

    first commit

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Aarti Singh",
+  },
+  {
+    label: "Age",
+    value: "22",
+  },
+  {
+    label: "Address",
+    value: "India",
+  },
+  {
+    label: "Email",
+    value: "Arti126181120@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "9541163850",
+  },
+];
+
+const jobSummary =
+  "I am Aarti Singh, currently in my final year at IIT Jammu, pursuing a degree in Electrical Engineering. I have a strong passion for web development and am constantly honing my skills in this field. Apart from web development, I am also deeply interested in software development. I actively work on improving my problem-solving abilities and have solved numerous DSA problems on platforms like GeeksforGeeks and LeetCode";
+
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Web Developer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,75 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "100",
+        },
+        {
+          skillName: "CSS",
+          percentage: "90",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+        {
+          skillName:"REACT",
+          percentage:"90"
+        }
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "Node",
+          percentage: "70",
+        },
+        {
+          skillName: "PYTHON",
+          percentage: "80",
+        },
+        {
+          skillName:"Express",
+          percentage:"90"
+        }
+        
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "80",
+        },
+        
+      ],
+
+    },
+    {
+      label:"Software",
+      data:[
+        {
+          skillName:"Power BI",
+          percentage:"90",
+
+        },
+        {
+          skillName:"Vs code",
+          percentage:"100"
+
+        },
+        {
+          skillName:"Jupyter Notebook",
+          percentage:"100"
+        }
+      ]
+    }
+  ];
